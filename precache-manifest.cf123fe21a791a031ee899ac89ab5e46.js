f350dc7e674a2ac59075159fd976733627602133
skhg <skhg@users.noreply.github.com>
2020-07-04 17:12:35 +0000

    deploy: 66def9ab6af748a72dbd76682d70048121efd89b

---
+++ precache-manifest.cf123fe21a791a031ee899ac89ab5e46.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c902b9dec8ce7358f3953a58ef057f4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "211dec2543ca486755bf",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7376d06263e72af3f0ee",
+    "url": "/static/js/2.4af40db4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4af40db4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "211dec2543ca486755bf",
+    "url": "/static/js/main.9b5443c6.chunk.js"
+  },
+  {
+    "revision": "c86ec8f92a6c7cc9d0b8",
+    "url": "/static/js/runtime-main.600f20a5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
